6ff21390c3fb128f43b5ddd06e2211751ebae8da
KColeman-bit <kjcole0811@gmail.com>
2022-03-17 09:51:03 -0500

    First Person Movement

--- Assets/Scripts/FirstPersonScript.cs
+++ Assets/Scripts/FirstPersonScript.cs
@@ -7,15 +7,16 @@ public class FirstPersonScript : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 2f;
-    public float gravity = 9.81f;
+    public float gravity = -9.81f;
     public float JumpHeight = 3f;
 
     public Transform GroundCheck;
     public float GroundDistance = 0.4f;
     public LayerMask GroundMask;
-
+    
     Vector3 velocity;
     bool isGrounded;
+    
 
     // Update is called once per frame
     void Update()
@@ -28,19 +29,20 @@ public class FirstPersonScript : MonoBehaviour
         }
 
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * velocity.x + transform.forward * velocity.z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            velocity = new Vector3(velocity.x,JumpHeight, velocity.z);
+            velocity = new Vector3(velocity.x, JumpHeight, velocity.z);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
     }
 }
